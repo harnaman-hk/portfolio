8bf5ac8a4da59cbcf58796769a9f151e21bbb26d
harnaman-hk <harnaman24@gmail.com>
2020-06-05 22:41:01 +0530

    reordered TLS to the top in settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -125,9 +125,9 @@ STATIC_URL = '/static/'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_USER_PASSWORD')
 
